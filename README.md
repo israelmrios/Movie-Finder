ae6a2798285dc40c91ec12c636da505b88c05842
Justin Barner <justinrb14@gmail.com>
2021-09-27 21:44:38 -0700

    bug fixes and improvements

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # Movie-Finder
+
+## Description
+    We devoloped a website that will allow you to easily find which streaming service a movie is on. Using HTML, CSS, Bulma, JS and Jquery our group created a simple yet very functional user interface. By first searching through an imbd fetch request to find the correct movie you will be shown a list of relevant searches to choose from. Once the user finds their movie then they can select it and will prompt them with links to the websites or an error message. The user can then search for a new movie or select a button.
+
+## Installation
+
+To install this project go to https://github.com/israelmrios/Movie-Finder and copy the SSH key to clone it to your terminal.
+
+## Usage
+
+Go to https://israelmrios.github.io/Movie-Finder/ and search any movie you want with the input region. Next, find the specificy movie you were looking for and click the button below it. If it is available on any of the top 6 streaming platforms a button will be displayed with a link to the movie.
+
+<img src="./assets/imgs/sc1.png" width="50%">
+<img src="./assets/imgs/sc2.png" width="50%">
+<img src="./assets/imgs/sc3.png" width="50%">
+
+
+## Credits
+
+    Aydin Macias
+
+
+    Carlos Hernandez
+
+
+    Isreal Rios
+
+
+    Justin Barner
+
+
+## Liscense
+
+    MIT License
+
+    Copyright (c) [year] [fullname]
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
+var JBKey = "f4ceaec9f5mshd595b95b571776ap16c077jsn471bb392e0d1"
+var CarlosKey = "f23c6f8922msh66c1e577a5d9a54p1f699djsn591541dac664"
+var IsrealKey = "68e56fb629mshd9bc9c2bab39abdp1e8f8ejsn5a5cdb6e24a2"
+var AydinKey = "5cfd71de82msh5e735751f442143p1e03bcjsn78a94c1d0a36"
+
 function getMovieList(e) {
-
-
-    console.log("SUBMIT!");
     e.preventDefault();
-    var omdbRequestURL = "https://omdbapi.com/?s=" + $("#user-search").val() + "&apikey=405ba6dc";
-    $("#movie-container").focus()
-
-    $("#movie-container").removeClass("hidden")
+    var omdbRequestURL = "https://omdbapi.com/?s=" + $("#user-search").val() + "&type=movie&apikey=405ba6dc";
+    $("#noResults").addClass("hidden");
+    $("#noResultOmdb").addClass("hidden");
+    $("#movie-container").focus();
+    $("#movie-container").removeClass("hidden");
     $("#results").addClass("hidden")
     fetch(omdbRequestURL)
         .then(function (res) {
@@ -14,11 +17,17 @@ function getMovieList(e) {
             return res.json();
         })
         .then(function (data) {
-            console.log(data);
-            console.log("Title:" + data.Search[0].Title + " " + data.Search[0].imdbID)
+            console.log(data.Response);
+            if (data.Response === "False") {
+                $("<div/>", {
+                    id: "noResultOmdb",
+                    class: "noResultz"
+                }).text("Sorry this movie is not available in any of the top 6 streaming services. Please try a different movie.").appendTo("#search-region");
+                return;
+            }
+
             if ($("#movie-container").length)
                 $("#movie-container").empty()
-
             // display results here
             // create HTML here
             $("<div/>", {
@@ -41,10 +50,19 @@ function getMovieList(e) {
                     id: "figure" + i,
                     class: "image is-4by3"
                 }).appendTo("#card-image" + i);
-                $("<img/>", {
-                    src: data.Search[i].Poster,
-                    alt: data.Search[i].Title
-                }).appendTo("#figure" + i);
+
+                if (data.Search[i].Poster === "N/A") {
+                    $("<img/>", {
+                        src: "./assets/imgs/spongemiboy.png",
+                        alt: data.Search[i].Title
+                    }).appendTo("#figure" + i);
+                    console.log("no pic")
+                } else {
+                    $("<img/>", {
+                        src: data.Search[i].Poster,
+                        alt: data.Search[i].Title
+                    }).appendTo("#figure" + i);
+                }
                 // content container
                 $("<div/>", {
                     id: "card-content" + i,
@@ -60,7 +78,7 @@ function getMovieList(e) {
                 }).text(data.Search[i].Title + " " + data.Search[i].Year).appendTo("#media" + i);
                 $("<button/>", {
                     id: "button" + i,
-                    
+
                     class: "button",
                     text: "This one!"
                 }).click({
@@ -69,8 +87,6 @@ function getMovieList(e) {
                     poster: data.Search[i].Poster
                 },
                     getServices).appendTo("#media" + i);
-
-
             }
         });
 }
@@ -86,7 +102,7 @@ function getServices(e) {
     // get plot point
     var plotURL = "http://omdbapi.com/?i=" + e.data.imdbID + "&plot=full&r=json&apikey=405ba6dc&"
     fetch(plotURL)
-        .then(function(res) {
+        .then(function (res) {
             return res.json();
         })
         .then(function (data) {
@@ -94,7 +110,7 @@ function getServices(e) {
                 id: "movie-text"
             }).text(data.Plot).appendTo("#description");
         })
-
+    console.log(e.data.poster)
     $("<img/>", {
         src: e.data.poster,
         alt: e.data.title
@@ -102,52 +118,59 @@ function getServices(e) {
     $("<h2/>").html(e.data.title).appendTo("#movie-title");
 
     fetch("https://gowatch.p.rapidapi.com/lookup/title/imdb_id", {
-	    method: "POST",
-	    headers: {
-	    "content-type": "application/x-www-form-urlencoded",
-	    "x-rapidapi-host": "gowatch.p.rapidapi.com",
-	    "x-rapidapi-key": "f23c6f8922msh66c1e577a5d9a54p1f699djsn591541dac664"
-	    },
-	    body: JSON.stringify({
-		    id: e.data.imdbID,
-		    type: "movie",
-		    country: "us"
-	    })
+        method: "POST",
+        headers: {
+            "content-type": "application/x-www-form-urlencoded",
+            "x-rapidapi-host": "gowatch.p.rapidapi.com",
+            "x-rapidapi-key": JBKey
+        },
+        body: JSON.stringify({
+            id: e.data.imdbID,
+            type: "movie",
+            country: "us"
+        })
     })
-    .then(function(res) {
-        console.log(res.status)
-        return res.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        parseResults(data.offers);
-    })
+        .then(function (res) {
+            console.log(res.status)
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            parseResults(data.offers);
+        })
 }
 
 function parseResults(servicesArray) {
     var service, serviceURL;
-    var buttonArray = [];
-    // write object with service names, for loop, compare, return object
-    for (var i = 0; i < servicesArray.length; i++) {
-        if ((servicesArray[i].buy === 0 && servicesArray[i].rent === 0 && servicesArray[i].channel === null) && servicesArray[i].collection_name !== "The") {
-            console.log(servicesArray[i].url)
-            service = servicesArray[i].url.split(".")[1]; // test this
-            service = service.charAt(0).toUpperCase() + service.slice(1);
-            console.log(service);
-            serviceURL = servicesArray[i].url;
-            $("<button/>", {
-                class: service,
-                id: "button" + i
-            }).html(service).val(serviceURL).click(link).appendTo("#stream-btns")
+    var result = false;
+    if (typeof servicesArray !== "undefined") {
+        for (var i = 0; i < servicesArray.length; i++) {
+            if ((servicesArray[i].buy === 0 && servicesArray[i].rent === 0 && servicesArray[i].channel === null) && servicesArray[i].collection_name !== "The") {
+                console.log(servicesArray[i].url)
+                service = servicesArray[i].url.split(".")[1];
+                service = service.charAt(0).toUpperCase() + service.slice(1);
+                console.log(service);
+                serviceURL = servicesArray[i].url;
+                $("<button/>", {
+                    class: service + " butt",
+                    id: "button" + i
+                }).html(service).val(serviceURL).click(link).appendTo("#stream-btns")
+                result = true;
+            }
         }
     }
+    if (!result) {
+        console.log("No movie found!");
+        $("#noResults").removeClass("hidden");
+    }
+
 }
 
 function link(e) {
     window.open(e.target.value, "_blank");
 }
 
-$("#return").click(function() {
+$("#return").click(function () {
     location.reload()
 });
 
@@ -155,3 +178,4 @@ $(function () {
     console.log("Ready!");
     $("#btn").click(getMovieList)
 });
+
